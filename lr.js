109efb6d75a63417a45ac47571c60793f50b4802
arzoo <arzoosehgal26@gmail.com>
2018-08-14 22:25:05 +0530

    questions

---
+++ lr.js
@@ -0,0 +1,69 @@
+var questions = [
+{
+  question: "Look at this series: 31, 29, 24, 22, 17, ... What number should come next?",
+    options: ["15","14","13","12"],
+    answer:1
+  
+},
+{
+  question: "FAG, GAF, HAI, IAH, ____",
+    options: ["JAK","HAL","HAK","JAI"],
+    answer:1
+  
+},
+{
+  question: "Statement: It is desirable to put the child in school at the age of 5 or so.
+          Assumptions:
+          1)At that age the child reaches appropriate level of development and is ready to learn.
+          2)The schools do not admit children after six years of age.",
+    options: ["Only assumption I is implicit","Only assumption II is implicit","Either I or II is implicit","Both I and II are implicit"],
+    answer: 1
+  
+},
+{
+  question: "Statement: Should there be students union in college/university?
+Arguments:
+1)No. This will create a political atmosphere in the campus.
+2)Yes, it is very necessary Students are future political leaders.",
+    options: ["Only argument I is strong","Only argument II is strong","Either I or II is strong","Both I and II are strong"],
+    answer:4
+  
+},
+{
+  question: "Introducing a man to her husband, a woman said, “His brother’s father is the only son of my grandfather.” How is the woman related to this man?",
+    options: ["Mother","Aunt","Sister","Daughter"],
+    answer:3
+  
+},
+{
+  question: " If  today is Thursday , after 730 days which will be the day of the week ?",
+    options: ["Thursday","Saturday","Friday","Monday"],
+    answer:2
+  
+},
+{
+  question: "Embarrass: Humiliate",
+    options: ["Enquire: Ask","Embezzle: Peculate","Gamble: Investment","Annoy: Exasperate"],
+    answer:4
+  
+},
+{
+  question: "In a certain code language if the word ‘MUSEUM’ is coded as ‘LSPAPG’, then how will the word ‘PALACE’ be coded in that language?",
+    options: ["OYIWXY","OYIXYW","IYXYWO","YXWYOI"],
+    answer:1
+  
+},
+{
+  question: " If 26th March 2006 is Friday, 18th May 2008 is comes on which day?",
+    options: ["Sunday","Saturday","Monday","Friday"],
+    answer:4
+  
+},
+
+{
+  question: "If A is the son of Q, Q and Y are sisters, Z is the mother of Y, P is the son of Z, then which of the following statements is correct ? ",
+    options: ["P is the maternal uncle of A"," P and Y are sisters"," A and P are cousins","None of the above"],
+    answer:1
+  
+},
+]
---
+++ qa.js
@@ -0,0 +1,62 @@
+var questions = [
+	{
+  question: "Two ships are sailing in the sea on the two sides of a lighthouse. The angle of elevation of the top of the lighthouse is observed from the ships are 30° and 45° respectively. If the lighthouse is 100 m high, the distance between the two ships is:",
+    options: ["	173 m","200m","273m","300m"],
+    answer:3
+  
+},
+{
+  question: "A train 125 m long passes a man, running at 5 km/hr in the same direction in which the train is going, in 10 seconds. The speed of the train is:",
+    options: ["45km/h","50km/h","55km/h","60km/h"],
+    answer:2
+  
+},
+{
+  question: "If a person walks at 14 km/hr instead of 10 km/hr, he would have walked 20 km more. The actual distance travelled by him is:    
+  options: ["50km","56km","70km","76km"],
+    answer:1
+  
+},
+{
+  question: "A and B invest in a business in the ratio 3 : 2. If 5% of the total profit goes to charity and A's share is Rs. 855, the total profit is:",
+    options: ["Rs. 1425","Rs. 1500","Rs. 1537.50","Rs. 1576"],
+    answer:2
+  
+},
+{
+  question: "From a group of 7 men and 6 women, five persons are to be selected to form a committee so that at least 3 men are there on the committee. In how many ways can it be done?",
+    options: ["564","635","645","756"],
+    answer:4
+  
+},
+{
+  question: "A motorboat, whose speed in 15 km/hr in still water goes 30 km downstream and comes back in a total of 4 hours 30 minutes. The speed of the stream (in km/hr) is:",
+    options: ["4","5","6","7"],
+    answer:2
+  
+},
+{
+  question: "In the first 10 overs of a cricket game, the run rate was only 3.2. What should be the run rate in the remaining 40 overs to reach the target of 282 runs?",
+    options: ["6.25","6.5","7.0","7.25"],
+    answer:1
+  
+},
+{
+  question: "A vendor bought toffees at 6 for a rupee. How many for a rupee must he sell to gain 20%?",
+    options: ["3","4","6","5"],
+    answer:4
+  
+},
+{
+  question: "Two dice are thrown simultaneously. What is the probability of getting two numbers whose product is even?",
+    options: ["0.5","0.75","0.25","0.375"],
+    answer:2
+  
+},
+{
+  question: "The difference between a two-digit number and the number obtained by interchanging the digits is 36. What is the difference between the sum and the difference of the digits of the number if the ratio between the digits of the number is 1 : 2 ?",
+    options: ["4","6","8","16"],
+    answer:3
+  
+},
+]
